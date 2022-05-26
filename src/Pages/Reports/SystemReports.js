d908537b1630a43d0ed651882cfc159f4c9a14eb
jash-05 <jash.sheth@gmail.com>
2022-05-25 19:03:41 -0700

    added API call in system reports

--- src/Pages/Reports/SystemReports.js
+++ src/Pages/Reports/SystemReports.js
@@ -1,37 +1,76 @@
-import React from 'react'
-import { Card, Container } from 'react-bootstrap'
-import ReportCard from './ReportCard'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Card, Container } from "react-bootstrap";
+import { REACT_APP_BASE_API_URL } from "../../config";
+import ReportCard from "./ReportCard";
+
+// {
+//     "eventParticipantsToMinRequirementsRatio": 0,
+//     "numberOfCancelledEvents": 0,
+//     "avgTotalParticipantsForFinishedEvents": 0,
+//     "percentageOfPaidEvents": 0.0,
+//     "numberOfCreatedEvents": 18,
+//     "numberOfFinishedEvents": 0
+// }
 
 const SystemReports = () => {
-    return (
-        <div>
-            <br />
-            <h3>System Reports</h3>
-            <br />
-            <br />
-            <br />
-            <Container>
-                <div className='report-wrapper'>
-                    <ReportCard title="Statistic 1" value="90" />
+	const [stat1, setStat1] = useState(0);
+	const [stat2, setStat2] = useState(0);
+	const [stat3, setStat3] = useState(0);
+	const [stat4, setStat4] = useState(0);
+	const [stat5, setStat5] = useState(0);
+	const [stat6, setStat6] = useState(0);
 
-                    <ReportCard title="Statistic 2" value="100" />
+	const getReport = async () => {
+		// try {
+		// 	const response = await axios.get(
+		// 		`${REACT_APP_BASE_API_URL}/systemReport/get/${localStorage.getItem(
+		// 			"virtualTime"
+		// 		)}`
+		// 	);
+		// 	console.log("Response: ", response);
+		// } catch (err) {
+		// 	console.log("Error: ", err);
+		// }
+	};
 
-                    <ReportCard title="Statistic 3" value="200" />
-                </div>
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
+	useEffect(() => {}, []);
 
-                <div className='report-wrapper'>
-                    <ReportCard title="Statistic 1" value="90" />
+	return (
+		<div>
+			<br />
+			<h3>System Reports</h3>
+			<br />
+			<br />
+			<br />
+			<Container>
+				<div className="report-wrapper">
+					<ReportCard
+						title="Number of created events"
+						value={stat1}
+					/>
 
-                    <ReportCard title="Statistic 2" value="100" />
-                </div>
-            </Container >
-        </div >
-    )
-}
+					<ReportCard
+						title="Percentage of paid events"
+						value={stat2}
+					/>
 
-export default SystemReports
+					<ReportCard title="Statistic 3" value="200" />
+				</div>
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+
+				<div className="report-wrapper">
+					<ReportCard title="Statistic 1" value="90" />
+
+					<ReportCard title="Statistic 2" value="100" />
+				</div>
+			</Container>
+		</div>
+	);
+};
+
+export default SystemReports;
